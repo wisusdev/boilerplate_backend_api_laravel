d25910a81c8f998fcb5e3503d59973c8f7694b2e
Jesus Avelar <wisusdev@gmail.com>
2023-11-17 17:53:39 -0600

    Add AuthController for API authentication

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request): JsonResponse
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|between:2,100',
+                'email' => 'required|string|email|max:100|unique:users',
+                'password' => 'required|string|confirmed|min:6',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }
+
+            $userData = User::create($request->except('pasword_confirmation'));
+            return response()->json(['message' => "User Added", 'userData' => $userData, "statusCode" => 200]);
+        } catch (Exception $error) {
+            return response()->json(['message' => $error->getMessage(), "statusCode" => 400]);
+        }
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function login(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        if (!$token = auth()->attempt($validator->validated())) {
+            return response()->json(['error' => 'Either email or password is wrong.'], 401);
+        }
+
+        return $this->createNewToken($token);
+    }
+
+    public function refresh() {
+        return $this->createNewToken(auth()->refresh());
+    }
+
+    public function logout(): JsonResponse
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'User successfully signed out']);
+    }
+
+    public function userProfile(): JsonResponse
+    {
+        return response()->json(auth()->user());
+    }
+
+    protected function createNewToken($token): JsonResponse
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+}
