f9d24a5053af3b53d082fe6fb2e5e3c3ac657818
Jesus Avelar <wisusdev@gmail.com>
2024-01-12 10:55:00 -0600

    Add password reset email template

---
+++ resources/views/mail/password_reset.blade.php
@@ -0,0 +1,4 @@
+<p>
+    You request password reset of your account. If you want to change password <a href="{{config('app.frontend_url')}}/auth/reset-password?token={{$token}}">click</a> here.
+    or, copy and paste the URL below into your web browser: {{config('app.frontend_url')}}/auth/reset-password?token={{$token}}
+</p>
