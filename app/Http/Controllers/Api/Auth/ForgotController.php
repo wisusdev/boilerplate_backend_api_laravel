a68a3fa4cc71a6e6e9fe2e92a136b70e4eafb01c
Jesus Avelar <wisusdev@gmail.com>
2024-01-17 22:37:31 -0600

    Refactor forgot password functionality to include validation and error handling

--- app/Http/Controllers/Api/Auth/ForgotController.php
+++ app/Http/Controllers/Api/Auth/ForgotController.php
@@ -7,35 +7,72 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 
 class ForgotController extends Controller
 {
     public function forgot(Request $request){
-		$this->validate($request, [
-			'email' => 'required|email|exists:users,email'
-		]);
+		try {
+			$validator = Validator::make($request->all(), [
+				'email' => 'required|email|exists:users,email'
+			]);
 
-		$email = $request->email;
+			if ($validator->fails()) {
+				return response()->json([
+					'errors' => [
+						'status' => '422',
+						'title'  => 'Validation Error',
+						'detail' => $validator->errors(),
+					]
+				], 422);
+			}
 
-		if(User::where('email', $email)->doesntExist()){
-			return response(['message' => 'User doesn\'t exist', 'user_found' => false], 404);
+			$email = $request->email;
+
+			$data = [
+				'errors' => [
+					'status' => '400',
+					'title'  => 'Bad Request',
+					'detail' => 'Invalid credentials',
+				]
+			];
+
+			if(User::where('email', $email)->doesntExist()){
+				return response()->json($data, 400);
+			} else {
+				$token = Str::random(10);
+
+				DB::table('password_reset_tokens')->updateOrInsert(['email' => $email], [
+					'token' => $token,
+					'created_at' => now()->addHours(6)
+				]);
+
+				// Send email
+				Mail::send('mail.password_reset', ['token' => $token], function($message) use($email){
+					$message->to($email);
+					$message->subject('Reset your password');
+				});
+
+				return response()->json([
+					'data' => [
+						'attributes' => [
+							'status' => 'Success',
+							'message' => 'Reset password link sent to your email',
+						]
+					]
+				], 200);
+			}
+
+		} catch (\Exception $error) {
+			return response()->json([
+				'errors' => [
+					'status' => '422',
+					'title'  => 'Unprocessable Entity',
+					'detail' => $error->getMessage(),
+				]
+			], 422);
 		}
-
-		$token = Str::random(10);
-
-		DB::table('password_reset_tokens')->updateOrInsert(['email' => $email], [
-			'token' => $token,
-			'created_at' => now()->addHours(6)
-		]);
-
-		// Send email
-		Mail::send('mail.password_reset', ['token' => $token], function($message) use($email){
-			$message->to($email);
-			$message->subject('Reset your password');
-		});
-
-		return response()->json(['message' => 'Check your email', 'user_found' => true], 200);
 	}
 
 	public function reset(Request $request)
