f1b2f5e696043a53c9ad1cd01881563bfb9b0f04
Jesús Avelar <wisusdev@gmail.com>
2024-03-07 08:10:14 -0600

    Agregada la clase JsonApiValidationErrorResponse para manejar respuestas de error de validación en formato JSON API.

---
+++ app/Http/Responses/JsonApiValidationErrorResponse.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
+
+class JsonApiValidationErrorResponse extends JsonResponse {
+
+    public function __construct(ValidationException $exception, int $status = 422)
+    {
+        $data = $this->formatJsonApiErrors($exception);
+        $headers = ['Content-Type' => 'application/vnd.api+json'];
+
+        parent::__construct($data, $status, $headers);
+    }
+
+    public function formatJsonApiErrors(ValidationException $exception) : array
+    {
+        $title = $exception->getMessage();
+
+        return [
+            'errors' => collect($exception->errors())
+                ->map(function ($message, $field) use ($title) {
+                    return [
+                        'title' => $title,
+                        'detail' => $message[0],
+                        'source' => [
+                            'pointer' => "/".str_replace(".", "/", $field)
+                        ]
+                    ];
+                })
+                ->values()
+        ];
+    }
+}
