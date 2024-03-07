cf5487edfccf7ea493ded6ce6229dee79d59b8e5
Jesús Avelar <wisusdev@gmail.com>
2024-03-07 08:13:17 -0600

    Agregado middleware para validar documentos JSON API

---
+++ app/Http/Middleware/ValidateJsonApiDocument.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ValidateJsonApiDocument
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if($request->isMethod('POST') || $request->isMethod('PATCH')) {
+            $request->validate([
+                'data' => ['required', 'array'],
+                'data.type' => ['required', 'string'],
+                'data.attributes' => ['required', 'array']
+            ]);
+        }
+
+        if($request->isMethod('PATCH')){
+            $request->validate([
+                'data.id' => ['required', 'string']
+            ]);
+        }
+
+        return $next($request);
+
+    }
+}
