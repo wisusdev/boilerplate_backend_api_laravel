b5e93472d4fd7f4bdb9b32976ddeea92e68d0c6d
Jesús Avelar <wisusdev@gmail.com>
2024-04-01 10:49:13 -0600

    Agregado RoleSeeder para crear roles y asignar permisos

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = [
+            'editor',
+            'subscriber',
+            'admin',
+        ];
+
+        foreach ($roles as $role) {
+            $newRole = Role::create(['name' => $role, 'guard_name' => 'api']);
+            if ($newRole->name === 'admin') {
+                $newRole->givePermissionTo(Permission::all());
+            }
+        }
+    }
+}
