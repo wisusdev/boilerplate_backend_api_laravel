bc0a1a2432eee851cdc866d4c36b3dca318115fb
Jesús Avelar <wisusdev@gmail.com>
2024-03-31 23:49:03 -0600

    Agregado middleware para validar documentos JSON API

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,8 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\ValidateJsonApiHeaders::class
+            \App\Http\Middleware\ValidateJsonApiHeaders::class,
+            \App\Http\Middleware\ValidateJsonApiDocument::class
         ],
     ];
 
