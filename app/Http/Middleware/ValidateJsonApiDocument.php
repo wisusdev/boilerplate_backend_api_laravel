796e6376688fa0d9a8a9379ba736661684ebafde
Jesús Avelar <wisusdev@gmail.com>
2024-03-31 23:48:28 -0600

    Añadir validación de atributos solo si no contiene 'relationships' en la URL

--- app/Http/Middleware/ValidateJsonApiDocument.php
+++ app/Http/Middleware/ValidateJsonApiDocument.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
 use Symfony\Component\HttpFoundation\Response;
 
 class ValidateJsonApiDocument
@@ -15,21 +17,25 @@ class ValidateJsonApiDocument
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if($request->isMethod('POST') || $request->isMethod('PATCH')) {
+        if ($request->isMethod('POST') || $request->isMethod('PATCH')) {
             $request->validate([
                 'data' => ['required', 'array'],
                 'data.type' => ['required', 'string'],
-                'data.attributes' => ['required', 'array']
+                'data.attributes' => [
+                    Rule::requiredIf(
+                        !Str::of($request->url())->contains('relationships')
+                    ),
+                    'array'
+                ]
             ]);
         }
 
-        if($request->isMethod('PATCH')){
+        if ($request->isMethod('PATCH')) {
             $request->validate([
                 'data.id' => ['required', 'string']
             ]);
         }
 
         return $next($request);
-
     }
 }
