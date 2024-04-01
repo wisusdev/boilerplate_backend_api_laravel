6ababcb5281e56e1f532d4f5459915ea005faca3
Jesús Avelar <wisusdev@gmail.com>
2024-04-01 10:49:05 -0600

    Agregado PermissionSeeder para crear permisos iniciales

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = [
+            // env file
+            'env:index',
+            'env:update',
+
+            // setting
+            'setting:index',
+            'setting:update',
+
+            // role
+            'role:index',
+            'role:create',
+            'role:update',
+            'role:delete',
+
+            // user
+            'user:index',
+            'user:show',
+            'user:create',
+            'user:update',
+            'user:delete',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission, 'guard_name' => 'api']);
+        }
+    }
+}
