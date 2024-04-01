f297a8ecf49bd5684a6781ebd916cc85b74e0520
Jesús Avelar <wisusdev@gmail.com>
2024-03-31 23:48:00 -0600

    Actualizar controlador de inicio de sesión de API para incluir validación de datos de solicitud y autenticación de usuario.

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -2,92 +2,60 @@
 
 namespace App\Http\Controllers\Api\Auth;
 
+use App\Models\User;
 use Carbon\Carbon;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
-	public function login(Request $request)
-	{
-		try {
-			$validator = Validator::make($request->all(), [
-				'email' => 'required|email',
-				'password' => 'required',
-			]);
 
-			if ($validator->fails()) {
-				return response()->json([
-					'errors' => [
-						'status' => '422',
-						'title'  => 'Validation Error',
-						'detail' => $validator->errors(),
-					]
-				], 422);
-			}
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
 
-			$data = [
-				'errors' => [
-					'status' => '400',
-					'title'  => 'Bad Request',
-					'detail' => 'Invalid credentials',
-				]
-			];
+    public function login(Request $request): JsonResponse
+    {
+        $request->validate([
+            'data.email' => ['required', 'email'],
+            'data.password' => ['required'],
+            'data.device_name' => ['required']
+        ]);
 
-			if (auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
-				$tokenResult = auth()->user()->createToken('authToken');
-				$token = $tokenResult->token;
-				$token->expires_at = Carbon::now()->addWeeks(1);
-				$token->save();
+        $user = User::whereEmail($request->email)->first();
 
-				$data = [
-					'data' => [
-						'type' => 'Bearer',
-						'id' => $tokenResult->accessToken,
-						'attributes' => [
-							'expires_at' => $tokenResult->token->expires_at,
-							'status' => 'Success',
-							'message' => 'Login success',
-						]
-					]
-				];
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => [__('auth.failed')]
+            ]);
+        }
 
-				return response()->json($data, 200);
-			}
+        $tokenResult = $user()->createToken('authToken');
+        $token = $tokenResult->token;
+        $token->expires_at = Carbon::now()->addWeeks(1);
+        $token->save();
 
-			return response()->json($data, 400);
-			
-		} catch (Exception $error) {
-			return response()->json([
-				'errors' => [
-					'status' => '422',
-					'title'  => 'Unprocessable Entity',
-					'detail' => $error->getMessage(),
-				]
-			], 422);
-		}
-	}
+        return response()->json([
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse($token->expires_at)->toDateTimeString()
+        ]);
+    }
 
-	public function logout(Request $request): JsonResponse
-	{
-		try {
-			$request->user()->token()->revoke();
-			return response()->json(['status' => true, 'message' => 'Successfully logged out'], 200);
-		} catch (Exception $error) {
-			return response()->json(['message' => $error->getMessage(), "statusCode" => 422]);
-		}
-	}
-
-	public function user(Request $request): JsonResponse
-	{
-		try {
-			return response()->json(['status' => true, 'data' => $request->user()], 200);
-		} catch (Exception $error) {
-			return response()->json(['message' => $error->getMessage(), "statusCode" => 422]);
-		}
-	}
+    public function logout(Request $request): JsonResponse
+    {
+        try {
+            $request->user()->token()->revoke();
+            return response()->json(['status' => true, 'message' => 'Successfully logged out'], 200);
+        } catch (Exception $error) {
+            return response()->json(['message' => $error->getMessage(), "statusCode" => 422]);
+        }
+    }
 }
