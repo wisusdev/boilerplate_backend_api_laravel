aa0049a33446c40d52c605ca46b75ab3eaa6a5cc
Jesús Avelar <wisusdev@gmail.com>
2024-04-01 12:45:38 -0600

    Añadir métodos index, show y update a SettingController

--- app/Http/Controllers/Api/SettingController.php
+++ app/Http/Controllers/Api/SettingController.php
@@ -3,9 +3,35 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Setting;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class SettingController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        $setting = Setting::all();
+        return response()->json(['data' => $setting]);
+    }
+
+    public function show(string $key): JsonResponse
+    {
+        $setting = Setting::where('key', $key)->first();
+        return response()->json(['data' => $setting]);
+    }
+
+    public function update(Request $request): JsonResponse
+    {
+        $request->validate([
+            'key' => 'required|string',
+            'value' => 'required|string'
+        ]);
+
+        $setting = Setting::where('key', $request->key)->first();
+        $setting->value = $request->value;
+        $setting->save();
+
+        return response()->json(['message' => 'Setting updated successfully']);
+    }
 }
