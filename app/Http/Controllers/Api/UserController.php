55a7987b7a1622800cad993cd608352a594a1cfb
Jesús Avelar <wisusdev@gmail.com>
2024-03-31 23:48:10 -0600

    Agregado controlador UserController para mostrar información del usuario

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function show(Request $request): JsonResponse
+    {
+        return response()->json(['status' => true, 'data' => $request->user()], 200);
+    }
+}
