1469543c1353536d335baaf6eede555473b80464
Jesús Avelar <wisusdev@gmail.com>
2024-04-07 22:23:23 -0600

    Añadir vista de verificación de correo electrónico

---
+++ resources/views/mail/email_verify.blade.php
@@ -0,0 +1,5 @@
+<h2 class="mb-4">Hola {{ $name }},</h2>
+<p class="m-0">Por favor, haz clic en el enlace de abajo para verificar tu dirección de correo electrónico.</p>
+<p class="m-0"><a href="{{ $url }}" class="button">Verificar correo electrónico</a></p>
+<p class="mb-3">Si tienes problemas para hacer clic en el botón "Verificar correo electrónico", copia y pega la URL de abajo en tu navegador web:</p>
+<p><a href="{{ $url }}">{{ $url }}</a></p>
