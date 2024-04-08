f112ad0ecc1c141ebdd147e7e9b36441d1b20a61
Jesús Avelar <wisusdev@gmail.com>
2024-04-07 22:21:57 -0600

    Actualizar controlador VerifyEmailController para enviar correo de verificación de email

--- app/Http/Controllers/Api/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Api/Auth/VerifyEmailController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Validation\ValidationException;
 
 class VerifyEmailController extends Controller
@@ -45,7 +48,26 @@ class VerifyEmailController extends Controller
             ]);
         }
 
-        $request->user()->sendEmailVerificationNotification();
+        $user = $request->user();
+
+        $name = $user->first_name;
+
+        $url = URL::temporarySignedRoute(
+            'verification.verify',
+            Carbon::now()->addMinutes(60),
+            [
+                'id' => $user->getKey(),
+                'hash' => sha1($user->getEmailForVerification())
+            ],
+            false
+        );
+
+		$url = config('app.frontend_url') . str_replace('/api/v1', '', $url);
+    
+        Mail::send('mail.email_verify', ['url' => $url, 'name' => $name], function ($message) use ($user) {
+            $message->to($user->email);
+            $message->subject('Verify your email address');
+        });
 
         return response()->json(['message' => 'Email verification link sent']);
     }
