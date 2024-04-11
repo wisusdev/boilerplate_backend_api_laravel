3ade449acc24d8d4934548d5828baa6ce7301f81
Jesús Avelar <wisusdev@gmail.com>
2024-04-10 21:44:08 -0600

    Actualizar vista de correo electrónico en resources/views/mail/password_reset.blade.php

--- resources/views/mail/password_reset.blade.php
+++ resources/views/mail/password_reset.blade.php
@@ -1,4 +1,4 @@
 <p>
-    You request password reset of your account. If you want to change password <a href="{{$url}}">click</a> here.
-    or, copy and paste the URL below into your web browser: <a href="{{$url}}">{{$url}}</a>
+    Has solicitado el restablecimiento de la contraseña de tu cuenta. Si deseas cambiar la contraseña, haz clic <a href="{{$url}}">aquí.</a>.
+    O, copia y pega la URL de abajo en tu navegador web: <a href="{{$url}}">{{$url}}</a>
 </p>
