2ff8a1b727decd530dcd74ba6c6982562cf2bc97
Jesús Avelar <wisusdev@gmail.com>
2024-04-14 23:38:34 -0600

    Actualizar controlador AccountController para agregar el método updateProfile y corregir el método profile

--- app/Http/Controllers/Api/AccountController.php
+++ app/Http/Controllers/Api/AccountController.php
@@ -3,16 +3,56 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AccountUpdateRequest;
+use App\Http\Resources\ProfileResource;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\ValidationException;
 
 class AccountController extends Controller
 {
-    public function profile(Request $request): JsonResponse
+    public function profile(Request $request): JsonResource
     {
-        return response()->json(['data' => $request->user()]);
+        return ProfileResource::make($request->user());
+    }
+
+    public function updateProfile(AccountUpdateRequest $request): JsonResource
+    {
+        $user = $request->user();
+
+        if ($request->has('data.attributes.avatar') && $request->input('data.attributes.avatar')) {
+            $destinationPath = '/uploads/' . date('Y') . '/' . date('m') . '/' . date('d');
+
+            if (Storage::disk('public')->exists($user->avatar)) {
+                Storage::disk('public')->delete($user->avatar);
+            }
+
+            $avatar = $request->input('data.attributes.avatar');
+            $avatar = str_replace('data:image/png;base64,', '', $avatar);
+            $avatar = str_replace(' ', '+', $avatar);
+            $avatar = base64_decode($avatar);
+            $avatarName = $destinationPath . '/avatar.webp';
+            Storage::disk('public')->put($avatarName, $avatar);
+        }
+
+        $user->update([
+            'first_name' => $request->input('data.attributes.first_name'),
+            'last_name' => $request->input('data.attributes.last_name'),
+            'email' => $request->input('data.attributes.email'),
+            'avatar' => $avatarName ?? $user->avatar,
+            'language' => $request->input('data.attributes.language')
+        ]);
+
+        if($user->email !== $request->input('data.attributes.email')) {
+            $user->email_verified_at = null;
+            $user->save(['timestamps' => false]);
+            $user->sendEmailVerificationNotification();
+        }
+
+        return ProfileResource::make($user);
     }
 
     public function devicesAuthList(Request $request): JsonResponse
@@ -41,18 +81,4 @@ class AccountController extends Controller
         return response()->json(['message' => 'Password changed successfully']);
     }
 
-    public function updateProfile(Request $request): JsonResponse
-    {
-        $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|email|unique:users,email,' . $request->user()->id
-        ]);
-
-        $user = $request->user();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->save();
-
-        return response()->json(['message' => 'Profile updated successfully']);
-    }
 }
