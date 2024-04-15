f5e91611bbb9826a45bfaf1b2c20294788de6928
Jesús Avelar <wisusdev@gmail.com>
2024-04-14 23:39:04 -0600

    Actualizar recurso ProfileResource para agregar el campo 'language' en toArray

---
+++ app/Http/Resources/ProfileResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\JsonApi\Traits\JsonApiResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProfileResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'profiles',
+            'id' => (string) $this->resource->getRouteKey(),
+            'attributes' => [
+                'first_name' => $this->resource->first_name,
+                'last_name' => $this->resource->last_name,
+                'username' => $this->resource->username,
+                'email' => $this->resource->email,
+                'avatar' => $this->resource->avatar ? asset('storage' . $this->resource->avatar) : null,
+                'language' => $this->resource->language,
+            ]
+        ];
+    }
+}
