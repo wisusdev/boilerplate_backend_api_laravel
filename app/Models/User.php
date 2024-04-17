f2ae6df492338ac4859fd0f779bc0796fbe335ef
Jesus Avelar <wisusdev@gmail.com>
2024-04-16 21:09:17 -0600

    Agregar relación HasMany para deviceInfos en User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use App\Notifications\VerifyEmail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -59,5 +60,10 @@ class User extends Authenticatable implements MustVerifyEmail
     public function getResourceType(): string
     {
         return 'users';
+    }
+
+    public function deviceInfos(): HasMany
+    {
+        return $this->hasMany(DeviceInfo::class);
     }
 }
