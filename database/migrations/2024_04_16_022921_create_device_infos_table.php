5e7fd95a889c494b588d5b8a88d0d1005705297d
Jesus Avelar <wisusdev@gmail.com>
2024-04-16 21:09:38 -0600

    Agregar migración para crear la tabla device_infos

---
+++ database/migrations/2024_04_16_022921_create_device_infos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('device_infos', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('user_id')->index();
+            $table->string('session_token');
+            $table->timestamp('login_at');
+            $table->string('browser');
+            $table->string('os');
+            $table->string('ip');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('device_infos');
+    }
+};
