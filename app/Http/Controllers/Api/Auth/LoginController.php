301d47221587e8add09be49866a7e58f7591bb5a
Jesus Avelar <wisusdev@gmail.com>
2024-04-17 23:17:12 -0600

    Actualizar controlador de inicio de sesión para obtener información del dispositivo en LoginController.php

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -30,14 +30,12 @@ class LoginController extends Controller
         $token->expires_at = Carbon::now()->addWeeks(1);
         $token->save();
 
-        $country = file_get_contents('http://ip-api.com/json/' . $request->ip() . '?fields=country');
-
         DeviceInfo::create([
             'login_at' => now(),
-            'browser' => $request->header('User-Agent'),
-            'os' => $request->server('HTTP_USER_AGENT'),
+            'browser' => $this->getBrowser($request->header('User-Agent')),
+            'os' => $this->getOS($request->server('HTTP_USER_AGENT')),
             'ip' => $request->ip(),
-            'country' => json_decode($country)->country ?? 'Unknown',
+            'country' => $this->getCountry($request->ip()),
             'user_id' => $user->id,
             'session_token' => $token->id,
         ]);
@@ -50,5 +48,106 @@ class LoginController extends Controller
         ];
 
         return LoginResource::make($dataResponse);
+    }
+
+    private function getOS($userAgent): string
+    {
+        $operativeSystems = [
+            'Windows NT 10.0' => 'Windows 10',
+            'Windows NT 6.2' => 'Windows 8',
+            'Windows NT 6.1' => 'Windows 7',
+            'Windows NT 6.0' => 'Windows Vista',
+            'Windows NT 5.1' => 'Windows XP',
+            'Windows NT 5.0' => 'Windows 2000',
+            'Mac' => 'Mac',
+            'Linux' => 'Linux',
+            'Debian' => 'Debian',
+            'Ubuntu' => 'Ubuntu',
+            'Chrome OS' => 'Chrome OS',
+            'PostmanRuntime' => 'Postman',
+            'Unknown' => 'Unknown OS'
+        ];
+
+        foreach ($operativeSystems as $os => $osName) {
+            if (false !== $pos = strpos($userAgent, $os)){
+                break;
+            }
+        }
+
+        return $osName;
+    }
+
+    private function getBrowser($userAgent): string
+    {
+        $trans = array(
+            'OPR' => 'Opera',
+            'UBrowser' => 'UC Browser',
+            'YaBrowser' => 'Yandex',
+            'Edg' => 'Edge',
+            'Edge' => 'Microsoft Edge',
+            'MSIE' => 'Internet Explorer',
+            // yes, I know Trident isn't IE but it's the string that identifies IE11
+            'Trident' => 'Internet Explorer 11.0',
+            'PostmanRuntime' => 'Postman'
+        );
+
+        $browsers = [
+            'Brave',
+            'Chrome',
+            'coc_coc_browser',
+            'Edg',
+            'Edge',
+            'Firefox',
+            'IEMobile',
+            'MSIEMobile',
+            'MSIE',
+            'Opera',
+            'Opera Mini',
+            'OPR',
+            'PaleMoon',
+            'PostmanRuntime',
+            'Safari',
+            'Trident',
+            'Trident/[.0-9]+',
+            'UCBrowser',
+            'Unknown',
+            'UBrowser',
+            'Vivaldi',
+            'Virgilio',
+            'YaBrowser'
+        ];
+
+        foreach ($browsers as $browser) {
+            if (false !== $pos = strpos($userAgent, $browser)){
+                break;
+            }
+        }
+
+        $sub1 = substr($userAgent, $pos);   // from browser name to end
+        $sub2 = explode(' ', $sub1);     // just browser name and version
+        $sub3 = explode('/', $sub2[0]);  // split browser name and version
+        $sub4 = explode('.', $sub3[1]);  // get sub, sub sub versions etc
+
+        $browserName = $browser;
+
+        if (isset($trans[$browser])) {
+            $browserName = $trans[$browser];
+        }
+
+        return $browserName;
+    }
+
+    private function getCountry($ip): string
+    {
+        $json = file_get_contents("http://ip-api.com/json/{$ip}?fields=country,city");
+        $data = json_decode($json, true);
+
+        $country = 'Unknown';
+
+        if(array_key_exists('city', $data) && array_key_exists('country', $data)) {
+            $country = $data['city'] . ', ' . $data['country'];
+        }
+
+        return $country;
     }
 }
