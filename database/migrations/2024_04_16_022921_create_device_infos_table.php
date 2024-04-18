64cf2904df1628c8c45d971d2c2d11f52923885c
Jesús Avelar <wisusdev@gmail.com>
2024-04-18 17:22:12 -0600

    Actualizar migración para crear la tabla device_infos

--- database/migrations/2024_04_16_022921_create_device_infos_table.php
+++ database/migrations/2024_04_16_022921_create_device_infos_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->string('os');
             $table->string('ip');
             $table->string('country');
-            $table->timestamps();
         });
     }
 
