1641230eced3810bae6fec4e3df5653b8b4b4c40
Jesús Avelar <wisusdev@gmail.com>
2024-04-19 22:44:41 -0600

    Agregar plantilla de correo para eliminar cuenta en delete_account.blade.php

---
+++ resources/views/mail/delete_account.blade.php
@@ -0,0 +1,4 @@
+<p>Hola {{ $name }},</p>
+<p>Has solicitado la eliminación de tu cuenta en {{ config('app.name') }}. Si no has sido tú, por favor, ignora este mensaje.</p>
+<p>Si has sido tú, por favor, haz clic en el enlace de abajo para confirmar la eliminación de tu cuenta.</p>
+<p><a href="{{ $url }}" class="button">Eliminar cuenta</a></p>
