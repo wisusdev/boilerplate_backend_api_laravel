ff204778397934f6e967fca3f330694e16337d19
Jesús Avelar <wisusdev@gmail.com>
2024-04-22 22:27:22 -0600

    Actualizar controlador de permisos para incluir la acción index en PermissionsController.php

---
+++ app/Http/Controllers/Api/PermissionsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Permission;
+use Illuminate\Http\JsonResponse;
+
+class PermissionsController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $permissions = Permission::get()->select(['uuid', 'name']);
+        return response()->json($permissions);
+    }
+}
