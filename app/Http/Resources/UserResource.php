e78958e56662d55aa38ab266634c18fefd05e7b1
Jesús Avelar <wisusdev@gmail.com>
2024-04-28 09:24:52 -0600

    Actualizar método toArray en UserResource.php para incluir el atributo 'email_verified_at'

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -2,25 +2,25 @@
 
 namespace App\Http\Resources;
 
-use App\JsonApi\Traits\JsonApiResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
-    use JsonApiResource;
-
-
-    public function toJsonApi(): array
+    public function toArray(Request $request): array
     {
         return [
-            'username' => $this->resource->username,
-            'first_name' => $this->resource->first_name,
-            'last_name' => $this->resource->last_name,
-            'email' => $this->resource->email,
-            'created_at' => $this->resource->created_at,
-            'updated_at' => $this->resource->updated_at,
-            'roles' => $this->resource->roles->pluck('name'),
+            'type' => 'users',
+            'id' => (string) $this->resource->getRouteKey(),
+            'attributes' => [
+                'username' => $this->resource->username,
+                'first_name' => $this->resource->first_name,
+                'last_name' => $this->resource->last_name,
+                'email' => $this->resource->email,
+                'email_verified_at' => $this->resource->email_verified_at,
+                'created_at' => $this->resource->created_at,
+                'updated_at' => $this->resource->updated_at,
+            ]
         ];
     }
 }
