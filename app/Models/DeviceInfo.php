e33cdd65e42a8b1133f8fe293d9b5b4a038fcca6
Jesús Avelar <wisusdev@gmail.com>
2024-05-01 14:16:22 -0600

    Actualizar método toArray en UserResource.php para incluir el atributo 'roles'

--- app/Models/DeviceInfo.php
+++ app/Models/DeviceInfo.php
@@ -2,15 +2,15 @@
 
 namespace App\Models;
 
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Date;
 
 class DeviceInfo extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'user_id',
