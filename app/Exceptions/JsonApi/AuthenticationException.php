df1c4a4224a34eed08551fbd892d6f6669df9d0b
Jesús Avelar <wisusdev@gmail.com>
2024-05-02 21:26:39 -0600

    Actualizar renderización de excepción de autenticación como respuesta JSON

--- app/Exceptions/JsonApi/AuthenticationException.php
+++ app/Exceptions/JsonApi/AuthenticationException.php
@@ -3,15 +3,15 @@
 namespace App\Exceptions\JsonApi;
 
 use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class AuthenticationException extends Exception
 {
     /**
      * Render the exception as an HTTP response.
      */
-    public function render(Request $request)
+    public function render(Request $request): JsonResponse
     {
         return response()->json([
             'errors' => [
