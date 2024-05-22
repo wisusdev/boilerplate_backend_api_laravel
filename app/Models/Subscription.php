4862e29c2a248f7b04d66c387b88f3f4c6d10115
Jesús Avelar <wisusdev@gmail.com>
2024-05-22 14:06:10 -0600

    feat: Add Subscription model and its relationships

---
+++ app/Models/Subscription.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Subscription extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'package_id',
+        'start_date',
+        'end_date',
+        'trial_ends_at',
+        'package_price',
+        'package_details',
+        'created_by',
+        'payment_method',
+        'payment_transaction_id',
+        'status',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function package(): BelongsTo
+    {
+        return $this->belongsTo(Package::class);
+    }
+
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function isApproved(): bool
+    {
+        return $this->status === 'approved';
+    }
+
+    public function isWaiting(): bool
+    {
+        return $this->status === 'waiting';
+    }
+
+    public function isDeclined(): bool
+    {
+        return $this->status === 'declined';
+    }
+
+    public function isCanceled(): bool
+    {
+        return $this->status === 'cancel';
+    }
+}
