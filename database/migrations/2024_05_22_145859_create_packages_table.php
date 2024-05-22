32b3eadfe72e8523e019e0c5f9240517dd2a45f1
Jesús Avelar <wisusdev@gmail.com>
2024-05-22 10:07:30 -0600

    feat: Add migration for creating packages table

---
+++ database/migrations/2024_05_22_145859_create_packages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('max_users');
+            $table->enum('interval', ['day', 'week', 'month', 'year']);
+            $table->integer('interval_count');
+            $table->decimal('price', 8, 2);
+            $table->integer('trial_days')->default(0);
+            $table->boolean('active')->default(true);
+            $table->foreignUuid('created_by')->constrained('users')->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('packages');
+    }
+};
