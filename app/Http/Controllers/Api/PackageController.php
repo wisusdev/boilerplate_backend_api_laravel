edf8061e09a91a17641e2173ab10e1b7fa194837
Jesús Avelar <wisusdev@gmail.com>
2024-05-23 23:09:03 -0600

    feat: Add PackageController for handling API requests related to packages

---
+++ app/Http/Controllers/Api/PackageController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PackageRequest;
+use App\Http\Resources\PackageResource;
+use App\Models\Package;
+
+class PackageController extends Controller
+{
+    public function index()
+    {
+        $packages = Package::query()
+            ->sparseFieldset()
+            ->jsonPaginate();
+
+        return PackageResource::collection($packages);
+    }
+
+    public function store(PackageRequest $request)
+    {
+        $data = $request->validated();
+        $package = Package::create($data['data']['attributes']);
+
+        return PackageResource::make($package);
+    }
+
+    public function show(Package $package): PackageResource
+    {
+        return PackageResource::make($package);
+    }
+
+    public function update(PackageRequest $request, Package $package): PackageResource
+    {
+        $data = $request->validated();
+        $package->update($data['data']['attributes']);
+
+        return PackageResource::make($package);
+    }
+
+    public function destroy(Package $package): void
+    {
+        $package->delete();
+    }
+}
