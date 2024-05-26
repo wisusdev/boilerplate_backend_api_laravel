569349e84bf619af77ce09802810f51388b0e3c7
Jesús Avelar <wisusdev@gmail.com>
2024-05-25 23:39:59 -0600

    feat: Update SubscriptionController to include package trial period in store method

--- app/Http/Controllers/Api/SubscriptionController.php
+++ app/Http/Controllers/Api/SubscriptionController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SubscriptionRequest;
 use App\Http\Resources\SubscriptionResource;
+use App\Models\Package;
 use App\Models\Subscription;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class SubscriptionController extends Controller
@@ -27,15 +29,52 @@ class SubscriptionController extends Controller
     public function store(SubscriptionRequest $request): SubscriptionResource
     {
         $data = $request->validated();
-        $subscription = Subscription::create($data['data']['attributes']);
+
+        $package = Package::find($data['data']['attributes']['package_id']);
+
+        $start_date = now();
+        $trial_days = $package->trial_days;
+
+        $end_date = match ($package->interval) {
+            'day' => $start_date->addDays($package->interval_count),
+            'week' => $start_date->addWeeks($package->interval_count),
+            'month' => $start_date->addMonths($package->interval_count),
+            'year' => $start_date->addYears($package->interval_count),
+            default => $start_date,
+        };
+
+        if($trial_days > 0) {
+            $end_date = $start_date->addDays($package->trial_days);
+        }
+
+        $subscription = Subscription::create([
+            'user_id' => $data['data']['attributes']['user_id'],
+            'package_id' => $package->id,
+            'start_date' => $start_date,
+            'end_date' => $end_date,
+            'trial_ends_at' => $trial_days > 0 ? $end_date : null,
+            'package_price' => $package->price,
+            'package_details' => $package->description,
+            'created_by' => $data['data']['attributes']['created_by'],
+            'payment_method' => $data['data']['attributes']['payment_method'],
+            'payment_transaction_id' => $data['data']['attributes']['payment_transaction_id'],
+            'status' => $data['data']['attributes']['status'],
+        ]);
 
         return SubscriptionResource::make($subscription);
     }
 
-    public function update(SubscriptionRequest $request, Subscription $subscription): SubscriptionResource
+    public function update(Request $request, Subscription $subscription): SubscriptionResource
     {
-        $data = $request->validated();
-        $subscription->update($data['data']['attributes']);
+        $data = $request->validate([
+            'data' => ['required', 'array'],
+            'data.type' => ['required', 'string', 'in:subscriptions'],
+            'data.attributes.status' => ['in:approved,waiting,declined,cancel'],
+        ]);
+
+        $subscription->update([
+            'status' => $data['data']['attributes']['status'],
+        ]);
 
         return SubscriptionResource::make($subscription);
     }
