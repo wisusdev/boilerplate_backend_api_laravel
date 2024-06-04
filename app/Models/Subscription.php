288684aee6a21c968305f0e9d15a1a4277a17328
Jesús Avelar <wisusdev@gmail.com>
2024-06-03 23:39:25 -0600

    feat: Add HasUuids trait to Subscription model

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,7 +10,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Subscription extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, HasUuids;
 
     protected $fillable = [
         'user_id',
