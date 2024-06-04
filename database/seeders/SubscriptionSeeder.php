71b41ea852a0e3f0911cb355365702c8b959a94a
Jesús Avelar <wisusdev@gmail.com>
2024-06-03 23:40:17 -0600

    feat: Update SubscriptionSeeder to use UUIDs for package_id and add package details

--- database/seeders/SubscriptionSeeder.php
+++ database/seeders/SubscriptionSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Package;
 use App\Models\Subscription;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -17,16 +18,19 @@ class SubscriptionSeeder extends Seeder
         $userId00 = User::where('username', 'user00')->first()->id;
         $userId01 = User::first()->id;
 
+        $package01 = Package::where('name', 'Basic')->first();
+        $package02 = Package::where('name', 'Pro')->first();
+        $package03 = Package::where('name', 'Enterprise')->first();
 
         $subscriptions = [
             [
                 'user_id' => $userId01,
-                'package_id' => 1,
+                'package_id' => $package01->id,
                 'start_date' => now(),
                 'end_date' => now()->addMonth(),
                 'trial_ends_at' => null,
-                'package_price' => 10.00,
-                'package_details' => 'Package details',
+                'package_price' => $package01->price,
+                'package_details' => $package01->description,
                 'created_by' => $userId00,
                 'payment_method' => 'stripe',
                 'payment_transaction_id' => 'stripe_transaction_id',
@@ -34,12 +38,12 @@ class SubscriptionSeeder extends Seeder
             ],
             [
                 'user_id' => $userId01,
-                'package_id' => 2,
+                'package_id' => $package02->id,
                 'start_date' => now(),
                 'end_date' => now()->addMonth(),
                 'trial_ends_at' => now()->addDays(7),
-                'package_price' => 50.00,
-                'package_details' => 'Package details',
+                'package_price' => $package02->price,
+                'package_details' => $package02->description,
                 'created_by' => $userId00,
                 'payment_method' => 'stripe',
                 'payment_transaction_id' => 'stripe_transaction_id',
@@ -47,12 +51,12 @@ class SubscriptionSeeder extends Seeder
             ],
             [
                 'user_id' => $userId01,
-                'package_id' => 3,
+                'package_id' => $package03->id,
                 'start_date' => now(),
                 'end_date' => now()->addMonth(),
                 'trial_ends_at' => now()->addDays(14),
-                'package_price' => 100.00,
-                'package_details' => 'Package details',
+                'package_price' => $package03->price,
+                'package_details' => $package03->description,
                 'created_by' => $userId00,
                 'payment_method' => 'stripe',
                 'payment_transaction_id' => 'stripe_transaction_id',
