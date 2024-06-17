4c7dd3c14e3eb4085645e013f8315148a7c46647
Jesús Avelar <wisusdev@gmail.com>
2024-06-16 23:09:12 -0600

    feat: Update PackageResource to include additional attributes

--- app/Http/Resources/PackageResource.php
+++ app/Http/Resources/PackageResource.php
@@ -20,7 +20,13 @@ class PackageResource extends JsonResource
             'attributes' => [
                 'name' => $this->resource->name,
                 'description' => $this->resource->description,
+                'max_users' => $this->resource->max_users,
+                'interval' => $this->resource->interval,
+                'interval_count' => $this->resource->interval_count,
                 'price' => $this->resource->price,
+                'trial_days' => $this->resource->trial_days,
+                'active' => $this->resource->active,
+                'created_by' => $this->resource->created_by,
                 'created_at' => $this->resource->created_at,
                 'updated_at' => $this->resource->updated_at,
             ],
