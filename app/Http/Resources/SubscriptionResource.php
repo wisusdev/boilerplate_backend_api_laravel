e5f4c8ac0508efc6cdb1cb9cbd66e5dd0b9b8963
Jesús Avelar <wisusdev@gmail.com>
2024-06-18 22:36:48 -0600

    feat: Update SubscriptionResource to include createdBy relationship and fix attribute naming

--- app/Http/Resources/SubscriptionResource.php
+++ app/Http/Resources/SubscriptionResource.php
@@ -16,42 +16,39 @@ class SubscriptionResource extends JsonResource
     {
         return [
             'type' => 'subscriptions',
-            'id' => (string) $this->resource->getRouteKey(),
+            'id' => (string)$this->resource->getRouteKey(),
             'attributes' => [
                 'start_date' => $this->resource->start_date,
                 'end_date' => $this->resource->end_date,
                 'trial_ends_at' => $this->resource->trial_ends_at,
                 'package_price' => $this->resource->package_price,
                 'package_details' => $this->resource->package_details,
-                'created_by' => $this->resource->created_by,
                 'payment_method' => $this->resource->payment_method,
                 'payment_transaction_id' => $this->resource->payment_transaction_id,
                 'status' => $this->resource->status,
             ],
             'relationships' => [
                 'user' => [
-                    'data' => [
-                        'type' => 'users',
-                        'id' => (string) $this->resource->user_id,
-                        'attributes' => [
-                            'username' => $this->resource->user->username,
-                            'first_name' => $this->resource->user->first_name,
-                            'last_name' => $this->resource->user->last_name,
-                            'email' => $this->resource->user->email,
-                        ],
+                    'type' => 'users',
+                    'id' => (string)$this->resource->user_id,
+                    'attributes' => [
+                        'username' => $this->resource->user->username,
+                        'first_name' => $this->resource->user->first_name,
+                        'last_name' => $this->resource->user->last_name,
+                        'email' => $this->resource->user->email,
                     ],
                 ],
-                'package' => [
-                    'data' => [
-                        'type' => 'packages',
-                        'id' => (string) $this->resource->package_id,
-                        'attributes' => [
-                            'name' => $this->resource->package->name,
-                            'description' => $this->resource->package->description,
-                            'price' => $this->resource->package->price
-                        ],
+                'createBy' => [
+                    'type' => 'users',
+                    'id' => (string)$this->resource->created_by,
+                    'attributes' => [
+                        'username' => $this->resource->createdBy->username,
+                        'first_name' => $this->resource->createdBy->first_name,
+                        'last_name' => $this->resource->createdBy->last_name,
+                        'email' => $this->resource->createdBy->email,
                     ],
                 ],
+                'package' => PackageResource::make($this->resource->package),
             ],
         ];
     }
