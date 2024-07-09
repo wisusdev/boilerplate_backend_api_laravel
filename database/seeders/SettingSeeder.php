c2e11cec55edbff269b1e93257f0d0c0894b44c8
Jesús Avelar <wisusdev@gmail.com>
2024-07-09 10:36:32 -0600

    feat: Update SettingSeeder to include additional settings

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -10,55 +10,111 @@ use Illuminate\Support\Str;
 
 class SettingSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        DB::table('settings')->delete();
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void
+	{
+		DB::table('settings')->delete();
 
-        $settings = [
-            ['key' => 'app_name', 'value' => config('app.name')],
-            ['key' => 'app_url', 'value' => config('app.url')],
+		$settings = [
+			['key' => 'app', 'value' => json_encode([
+				"name" => config('app.name'),
+				"url" => config('app.url'),
+				"api_url" => config('app.frontend_url'),
+				"description" => "Laravel api, Angular frontend and Flutter mobile.",
+				"logo" => "logo.png",
+				"favicon" => "favicon.png",
+				"email" => "info@wisus.dev",
+				"phone" => "+503 1234-5678",
+				"address" => "1234 Main St, San Salvador, El Salvador",
+				"timezone" => "America/El_Salvador",
+			])],
 
-            ['key' => 'currency', 'value' => 'USD'],
-            ['key' => 'currency_symbol', 'value' => '$'],
+			['key' => 'payment_info', 'value' => json_encode([
+				"currency" => "USD",
+				"currency_symbol" => "$",
+				"decimal_separator" => ".",
+				"thousands_separator" => ",",
+				"paypal" => [
+					"mode" => "sandbox",
+					"client_id" => "",
+					"client_secret" => "",
+					"active" => true
+				],
+				"stripe" => [
+					"mode" => "sandbox",
+					"key" => "",
+					"secret" => "",
+					"active" => false
+				],
+				"wompi" => [
+					"mode" => "sandbox",
+					"key" => "",
+					"secret" => "",
+					"active" => false
+				],
+				"serfinsa" => [
+					"mode" => "sandbox",
+					"key" => "",
+					"secret" => "",
+					"active" => false
+				],
+				"n1co" => [
+					"mode" => "sandbox",
+					"key" => "",
+					"secret" => "",
+					"active" => false
+				]
+			])],
 
-            ['key' => 'tax', 'value' => '0'],
-            ['key' => 'shipping_cost', 'value' => '0'],
+			['key' => 'taxes', 'value' => json_encode([
+				"iva" => 13,
+				"other" => 0
+			])],
 
-            ['key' => 'paypal_mode', 'value' => 'sandbox'],
-            ['key' => 'paypal_client_id', 'value' => ''],
-            ['key' => 'paypal_secret', 'value' => ''],
-            ['key' => 'paypal_status', 'value' => 'active'],
+			['key' => 'shipping', 'value' => json_encode([
+				"delivery_time" => "1-3 days",
+				"cost" => 5
+			])],
 
-            ['key' => 'stripe_mode', 'value' => 'sandbox'],
-            ['key' => 'stripe_key', 'value' => ''],
-            ['key' => 'stripe_secret', 'value' => ''],
-            ['key' => 'stripe_status', 'value' => 'inactive'],
+			['key' => 'social_login', 'value' => json_encode([
+				"facebook" => [
+					"client_id" => "",
+					"client_secret" => ""
+				],
+				"google" => [
+					"client_id" => "",
+					"client_secret" => ""
+				],
+				"twitter" => [
+					"client_id" => "",
+					"client_secret" => ""
+				],
+				"linkedin" => [
+					"client_id" => "",
+					"client_secret" => ""
+				]
+			])],
 
-            ['key' => 'wompi_mode', 'value' => 'sandbox'],
-            ['key' => 'wompi_key', 'value' => ''],
-            ['key' => 'wompi_secret', 'value' => ''],
-            ['key' => 'wompi_status', 'value' => 'inactive'],
+			['key' => 'mail', 'value' => json_encode([
+				"driver" => config('mail.default'),
+				"host" => config('mail.mailers.smtp.host'),
+				"port" => config('mail.mailers.smtp.port'),
+				"encryption" => config('mail.mailers.smtp.encryption'),
+				"username" => config('mail.mailers.smtp.username'),
+				"password" => config('mail.mailers.smtp.password'),
+				"from_address" => config('mail.from.address'),
+				"from_name" => config('mail.from.name'),
+			])],
+		];
 
-            ['key' => 'serfinsa_mode', 'value' => 'sandbox'],
-            ['key' => 'serfinsa_key', 'value' => ''],
-            ['key' => 'serfinsa_secret', 'value' => ''],
-            ['key' => 'serfinsa_status', 'value' => 'inactive'],
-
-            ['key' => 'n1co_mode', 'value' => 'sandbox'],
-            ['key' => 'n1co_key', 'value' => ''],
-            ['key' => 'n1co_secret', 'value' => ''],
-            ['key' => 'n1co_status', 'value' => 'inactive'],
-        ];
-
-        foreach ($settings as $setting) {
-            Setting::create([
-                'id' => Str::uuid(),
-                'key' => $setting['key'],
-                'value' => $setting['value'],
-            ]);
-        }
-    }
+		foreach ($settings as $setting) {
+			Setting::create([
+				'id' => Str::uuid(),
+				'key' => $setting['key'],
+				'value' => $setting['value'],
+			]);
+		}
+	}
 }
