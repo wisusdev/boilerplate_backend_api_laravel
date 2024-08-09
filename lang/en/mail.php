d180ef34d23eb47ca53015ec3678d6ef4f5a1697
Jesús Avelar <wisusdev@gmail.com>
2024-08-08 22:11:52 -0600

    feat: Add email templates for new subscriptions in multiple languages

---
+++ lang/en/mail.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	'greeting' => 'Hello :name,',
+	'subscription_thank_you' => 'Thank you for subscribing!',
+	'subscription_details' => 'Here are some important details about your subscription:',
+	'start_date' => 'Start Date:',
+	'plan_name' => 'Plan:',
+	'price' => 'Price:',
+	'support_contact' => 'If you have any questions or need assistance, please contact our support team.',
+	'regards' => 'Regards,',
+	'new_subscription_subject' => 'Welcome :name! Your subscription to :plan is confirmed',
+];
---
+++ lang/es/mail.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	'greeting' => 'Hola :name,',
+	'subscription_thank_you' => '¡Gracias por suscribirte!',
+	'subscription_details' => 'A continuación, encontrarás algunos detalles importantes sobre tu suscripción:',
+	'start_date' => 'Fecha de inicio:',
+	'plan_name' => 'Plan:',
+	'price' => 'Precio:',
+	'support_contact' => 'Si tienes alguna pregunta o necesitas asistencia, no dudes en ponerte en contacto con nuestro equipo de soporte.',
+	'regards' => 'Saludos,',
+	'new_subscription_subject' => '¡Bienvenido :name! Tu suscripción a :plan está confirmada',
+];
