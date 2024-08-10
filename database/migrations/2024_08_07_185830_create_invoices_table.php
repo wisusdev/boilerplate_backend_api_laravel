b2a9ba308908a052799ff62d29c335afdd4eeeba
Jesús Avelar <wisusdev@gmail.com>
2024-08-09 22:03:36 -0600

    feat: Add invoices and invoice items tables

---
+++ database/migrations/2024_08_07_185830_create_invoices_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+			$table->foreignUuid('user_id')->constrained()->onDelete('cascade');
+			$table->uuid('created_by')->index();
+			$table->string('invoice_number');
+			$table->date('invoice_date');
+			$table->date('due_date')->nullable();
+			$table->decimal('total_amount', 10, 2);
+			$table->enum('status', ['paid', 'unpaid', 'partial'])->default('unpaid');
+			$table->softDeletes();
+            $table->timestamps();
+        });
+
+		Schema::create('invoice_items', function (Blueprint $table) {
+			$table->uuid('id')->primary();
+			$table->foreignUuid('invoice_id')->constrained()->onDelete('cascade');
+			$table->string('description');
+			$table->integer('quantity');
+			$table->decimal('unit_price', 10, 2);
+			$table->decimal('total_price', 10, 2);
+			$table->json('metadata')->nullable();
+			$table->timestamps();
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
