25a1cd69b54dab0eca7f9fbe0c867bbe391f58e3
Jesús Avelar <wisusdev@gmail.com>
2024-08-11 23:06:03 -0600

    feat: Update SettingController to handle image uploads and optimize file storage

--- app/Http/Controllers/Api/SettingController.php
+++ app/Http/Controllers/Api/SettingController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SettingRequest;
+use App\Http\Resources\SettingResource;
 use App\Models\Setting;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Imagick\Driver;
 
 class SettingController extends Controller
 {
@@ -15,28 +18,56 @@ class SettingController extends Controller
         $this->middleware('can:settings:update')->only('update');
     }
     
-    public function index(): JsonResponse
+    public function index(): SettingResource
     {
 		$query = Setting::query()
 			->allowedFilters(['key'])
 			->sparseFieldset()
 			->first();
 
-		return response()->json(['data' => json_decode($query->value, true)]);
+		return SettingResource::make($query);
     }
 
 
-    public function update(Request $request): JsonResponse
-    {
-        $request->validate([
-            'key' => 'required|string',
-            'value' => 'required|string'
-        ]);
+	public function update(SettingRequest $request, Setting $setting): SettingResource
+	{
+		$dataValidated = $request->validated();
+		$attributes = (object) $dataValidated['data']['attributes'];
+		$settingsValue = json_decode($setting->value, true);
 
-        $setting = Setting::where('key', $request->key)->first();
-        $setting->value = $request->value;
-        $setting->save();
+		$files = [];
+		
+		if ($dataValidated['data']['type'] === 'app') {
+			$files = ['logo', 'favicon'];
+		}
 
-        return response()->json(['message' => 'Setting updated successfully']);
-    }
+		foreach ($files as $file) {
+			$fileApp = $attributes->$file;
+
+			if (preg_match('/^data:image\/(\w+);base64,/', $fileApp)) {
+				if (property_exists($attributes, $file) && $attributes->$file) {
+					if (isset($settingsValue[$file]) && Storage::disk('public')->exists($settingsValue[$file])) {
+						Storage::disk('public')->delete($settingsValue[$file]);
+					}
+
+					$fileApp = preg_replace('/^data:image\/\w+;base64,/', '', $fileApp);
+					$fileApp = str_replace(' ', '+', $fileApp);
+					$fileApp = base64_decode($fileApp);
+
+					$image = new ImageManager(new Driver());
+					$image = $image->read($fileApp)->toWebp(90);
+					$fileName = config('app.destination_path') . '/' . $file . '.webp';
+					Storage::disk('public')->put($fileName, (string) $image);
+
+					$attributes->$file = $fileName;
+				}
+			}
+		}
+
+		$setting->update([
+			'value' => json_encode($attributes)
+		]);
+
+		return SettingResource::make($setting);
+	}
 }
