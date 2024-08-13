13c3a7d843940a2fe73c68ccf24e96a481f53024
Jesús Avelar <wisusdev@gmail.com>
2024-08-12 22:44:27 -0600

    feat: Update SettingResource to include additional attributes

--- app/Http/Resources/SettingResource.php
+++ app/Http/Resources/SettingResource.php
@@ -14,10 +14,23 @@ class SettingResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+		$value = json_decode($this->resource->value, true);
+
         $data = [
 			'type' => $this->resource->key,
 			'id' => (string) $this->resource->getRouteKey(),
-			'attributes' => json_decode($this->resource->value, true)
+			'attributes' => [
+				'name' => $value['name'] ?? null,
+				'url_api' => $value['url_api'] ?? null,
+				'url_frontend' => $value['url_frontend'] ?? null,
+				'description' => $value['description'] ?? null,
+				'logo' => $value['logo'] ? asset('storage' . $value['logo']) : null,
+				'favicon' => $value['favicon'] ? asset('storage' . $value['favicon']) : null,
+				'email' => $value['email'] ?? null,
+				'phone' => $value['phone'] ?? null,
+				'address' => $value['address'] ?? null,
+				'timezone' => $value['timezone'] ?? null,
+			]
 		];
 
 		if($this->resource->key === 'app') {
