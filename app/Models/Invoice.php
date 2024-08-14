c73247010fa2ea82fbc3eb212684a944c58c636e
Jesús Avelar <wisusdev@gmail.com>
2024-08-13 22:21:44 -0600

    feat: Add fillable fields to Invoice model

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Invoice extends Model
 {
     use HasFactory;
+
+	protected $fillable = [
+		'user_id',
+		'created_by',
+		'invoice_number',
+		'invoice_date',
+		'due_date',
+		'total_amount',
+		'status',
+	];
 }
