333a2fca582c83d58809ae9ff6cb0e8dfe7625d8
Jesús Avelar <wisusdev@gmail.com>
2024-08-14 21:38:42 -0600

    feat: Add soft deletes and UUIDs to Invoice model

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Invoice extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, HasUuids;
 
 	protected $fillable = [
 		'user_id',
@@ -18,4 +22,14 @@ class Invoice extends Model
 		'total_amount',
 		'status',
 	];
+
+	public function items(): HasMany
+	{
+		return $this->hasMany(InvoiceItem::class);
+	}
+
+	public function user(): BelongsTo
+	{
+		return $this->belongsTo(User::class);
+	}
 }
