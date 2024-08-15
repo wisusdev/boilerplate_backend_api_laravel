e1f73399f4c43bf5d5ee0fc41ec84bfbe57259a3
Jesús Avelar <wisusdev@gmail.com>
2024-08-14 21:37:07 -0600

    feat: Update invoices table migration to use foreign key constraint for created_by field

--- database/migrations/2024_08_07_185830_create_invoices_table.php
+++ database/migrations/2024_08_07_185830_create_invoices_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('invoices', function (Blueprint $table) {
             $table->uuid('id')->primary();
 			$table->foreignUuid('user_id')->constrained()->onDelete('cascade');
-			$table->uuid('created_by')->index();
+			$table->foreignUuid('created_by')->index();
 			$table->string('invoice_number');
 			$table->date('invoice_date');
 			$table->date('due_date')->nullable();
