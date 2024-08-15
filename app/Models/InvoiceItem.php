33f43b2ba4539b63ff2915366be7f6d4d226d929
Jesús Avelar <wisusdev@gmail.com>
2024-08-14 21:39:54 -0600

    feat: Add InvoiceItem model for handling invoice item data

---
+++ app/Models/InvoiceItem.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class InvoiceItem extends Model
+{
+    use HasFactory, SoftDeletes, HasUuids;
+
+	protected $fillable = [
+		'invoice_id',
+		'description',
+		'quantity',
+		'unit_price',
+		'total_price',
+		'metadata',
+	];
+
+	public function invoice(): BelongsTo
+	{
+		return $this->belongsTo(Invoice::class);
+	}
+}
