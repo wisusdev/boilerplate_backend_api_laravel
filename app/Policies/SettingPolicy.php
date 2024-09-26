842cbd18f92fb750806838877280d0666466d8cf
Jesús Avelar <wisusdev@gmail.com>
2024-09-25 22:18:50 -0600

    Metodos en SettingPolicy
    Se añadieron metodos para validar los distintos permisos para settings

--- app/Policies/SettingPolicy.php
+++ app/Policies/SettingPolicy.php
@@ -8,8 +8,42 @@ use Illuminate\Auth\Access\Response;
 
 class SettingPolicy
 {
-	public function app(User $user, Setting $setting): bool
+	public function index(User $user): bool
+	{
+		return $user->hasPermissionTo('settings:index');
+	}
+	public function app(User $user): bool
 	{
 		return $user->hasPermissionTo('settings:app');
+	}
+
+	public function taxes(User $user): bool
+	{
+		return $user->hasPermissionTo('settings:taxes');
+	}
+
+	public function shipping(User $user): bool
+	{
+		return $user->hasPermissionTo('settings:shipping');
+	}
+
+	public function socialAuthServices(User $user): bool
+	{
+		return $user->hasPermissionTo('settings:social_auth_services');
+	}
+
+	public function email(User $user): bool
+	{
+		return $user->hasPermissionTo('settings:email');
+	}
+
+	public function pushNotification(User $user): bool
+	{
+		return $user->hasPermissionTo('settings:push_notification');
+	}
+
+	public function paymentGateway(User $user): bool
+	{
+		return $user->hasPermissionTo('settings:payment_gateway');
 	}
 }
