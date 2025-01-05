81e1c58e63e5f912f6cf14fdaf3f095e28b0cc98
Jesús Avelar <wisusdev@gmail.com>
2025-01-04 21:20:54 -0600

    Refactor plantilla de correo de nueva suscripción para utilizar la estructura de diseño

--- resources/views/mail/new_subscription.blade.php
+++ resources/views/mail/new_subscription.blade.php
@@ -1,10 +1,13 @@
-<p>{{ __('mail.greeting', ['name' => $name]) }}</p>
-<p>{{ __('mail.subscription_thank_you') }}</p>
-<p>{{ __('mail.subscription_details') }}</p>
-<ul>
-    <li><strong>{{ __('mail.start_date') }}</strong> {{ $start_date }}</li>
-    <li><strong>{{ __('mail.plan_name') }}</strong> {{ $plan_name }}</li>
-    <li><strong>{{ __('mail.price') }}</strong> {{ $price }}</li>
-</ul>
-<p>{{ __('mail.support_contact') }}</p>
-<p>{{ __('mail.regards') }}<br>{{ config('app.name') }}</p>
+@extends('layouts.email')
+@section('content')
+    <p>{{ __('mail.greeting', ['name' => $name]) }}</p>
+    <p>{{ __('mail.subscription_thank_you') }}</p>
+    <p>{{ __('mail.subscription_details') }}</p>
+    <ul>
+        <li><strong>{{ __('mail.start_date') }}</strong> {{ $start_date }}</li>
+        <li><strong>{{ __('mail.plan_name') }}</strong> {{ $plan_name }}</li>
+        <li><strong>{{ __('mail.price') }}</strong> {{ $price }}</li>
+    </ul>
+    <p>{{ __('mail.support_contact') }}</p>
+    <p>{{ __('mail.regards') }}<br>{{ config('app.name') }}</p>
+@endsection
