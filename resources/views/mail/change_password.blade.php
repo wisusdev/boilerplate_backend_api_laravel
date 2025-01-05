915bdd31894e7ad87f9a6455d96c1d49f7f6acd1
Jesús Avelar <wisusdev@gmail.com>
2025-01-04 21:21:07 -0600

    Refactor plantilla de correo de cambio de contraseña para utilizar la estructura de diseño

--- resources/views/mail/change_password.blade.php
+++ resources/views/mail/change_password.blade.php
@@ -1,5 +1,7 @@
-<p>Hola {{ $name }},</p>
-<p>Para cambiar tu contraseña, haz clic en el siguiente enlace:</p>
+@extends('layouts.email')
+@section('content')
+
+<p>Hola {{ $name }}, para cambiar tu contraseña, haz clic en el siguiente enlace:</p>
 <p><a href="{{ $url }}">{{ $url }}</a></p>
 <p>Si no has solicitado este cambio, ignora este mensaje.</p>
 <p>Saludos,<br>
