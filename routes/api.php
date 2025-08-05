a6a3977c49885e184c41b19931abe906566af396
Jesús Avelar <wisusdev@gmail.com>
2025-08-04 22:02:40 -0600

    Refactor rutas de API: reorganizar controladores y ajustar middleware para mejorar la estructura y seguridad

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,12 @@ use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\LogoutController;
 use App\Http\Controllers\Api\Auth\RefreshTokenController;
 use App\Http\Controllers\Api\Auth\RegisterController;
-use App\Http\Controllers\Api\AccountController;
 use App\Http\Controllers\Api\Auth\SocialAuthController;
 use App\Http\Controllers\Api\Auth\VerifyEmailController;
-use App\Http\Controllers\Api\PermissionsController;
-use App\Http\Controllers\Api\RolesController;
-use App\Http\Controllers\Api\SettingController;
-use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\Base\AccountController;
+use App\Http\Controllers\Api\Base\PermissionsController;
+use App\Http\Controllers\Api\Base\RolesController;
+use App\Http\Controllers\Api\Base\UserController;
 use App\Http\Middleware\ValidateJsonApiDocument;
 use Illuminate\Support\Facades\Route;
 
@@ -21,45 +20,41 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
+| routes are loaded by the RouteServiceProvider, and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
 */
 
 // Auth
-Route::withoutMiddleware([ValidateJsonApiDocument::class])->prefix('auth')->group(function () {
+Route::prefix('auth')->withoutMiddleware([ValidateJsonApiDocument::class])->group(function () {
     Route::post('/login', [LoginController::class, 'login'])->name('auth.login');
     Route::post('/register', [RegisterController::class, 'register'])->name('auth.register');
-    Route::post('/logout', [LogoutController::class, 'logout'])->name('auth.logout');
+    Route::post('/logout', [LogoutController::class, 'logout'])->name('auth.logout')->middleware('auth:api');
     Route::post('/forgot-password', [ForgotController::class, 'forgot'])->name('auth.forgot');
     Route::post('/reset-password', [ForgotController::class, 'reset'])->name('auth.reset');
-    Route::post('/email/resend', [VerifyEmailController::class, 'resend'])->name('verification.send');
+    Route::post('/email/resend', [VerifyEmailController::class, 'resend'])->name('verification.send')->middleware('auth:api');
     Route::get('/email/verify/{id}/{hash}', [VerifyEmailController::class, 'verifyEmail'])->name('verification.verify');
-    Route::get('/oauth/{driver}', [SocialAuthController::class, 'redirectToProvider'])->name('social.oauth');
-    Route::get('/oauth/{driver}/callback', [SocialAuthController::class,'handleProviderCallback'])->name('social.callback');
-    Route::middleware(['auth:api'])->post('/refresh-token', [RefreshTokenController::class])->name('auth.refresh-token');
+    Route::post('/refresh-token', [RefreshTokenController::class, 'refreshToken'])->name('auth.refresh-token')->middleware('auth:api');
+    Route::post('/verify-social-token', [SocialAuthController::class, 'verifySocialToken'])->name('auth.verify-social-token');
 });
 
 // Protected routes
 Route::middleware(['auth:api'])->group(function () {
-    // Users
-    Route::apiResource('/users', UserController::class);
-
-    // Settings
-    Route::apiResource('/settings', SettingController::class)->only(['index', 'show', 'update']);
+    // Permissions
+    Route::get('/permissions', [PermissionsController::class, 'index'])->name('permissions.index');
 
     // Roles
     Route::apiResource('/roles', RolesController::class);
 
-    // Permissions
-    Route::get('/permissions', [PermissionsController::class, 'index'])->name('permissions.index');
+    // Users
+    Route::apiResource('/users', UserController::class);
 
     // Account
-    Route::get('/account/profile', [AccountController::class, 'profile'])->name('profile.profile');
-    Route::patch('/account/profile', [AccountController::class, 'updateProfile'])->name('profile.update-profile');
-    Route::patch('/account/change-password', [AccountController::class, 'changePassword'])->name('profile.change-password');
-    Route::get('/account/devices-auth-list', [AccountController::class, 'devicesAuthList'])->name('profile.devices-auth-list');
-    Route::withoutMiddleware(ValidateJsonApiDocument::class)->post('/account/logout-device', [AccountController::class, 'logoutDevice'])->name('profile.logout-device');
-	Route::post('/account/delete-account', [AccountController::class, 'deleteAccount'])->name('profile.delete-account');
-    Route::delete('/account/delete-account-verify', [AccountController::class, 'deleteAccountVerify'])->name('profile.delete-account-verify');
+    Route::prefix('account')->group(function () {
+        Route::get('/profile', [AccountController::class, 'profile'])->name('account.profile');
+        Route::patch('/profile', [AccountController::class, 'updateProfile'])->name('account.update-profile');
+        Route::patch('/change-password', [AccountController::class, 'changePassword'])->name('account.change-password');
+        Route::post('/delete-account', [AccountController::class, 'deleteAccount'])->name('account.delete-account');
+        Route::delete('/delete-account-verify', [AccountController::class, 'deleteAccountVerify'])->name('account.delete-account-verify');
+    });
 });
